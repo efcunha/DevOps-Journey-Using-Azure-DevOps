677ac0fb4375bf39e97727b59269db3a08e2707d
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-09-13 09:31:22 -0400

    update

--- labs/1-Initial-Setup/scripts/create-terraform-storage.sh
+++ labs/1-Initial-Setup/scripts/create-terraform-storage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 RESOURCE_GROUP_NAME="devops-journey-rg"
-STORAGE_ACCOUNT_NAME="devopsjourneyazuredevops"
+STORAGE_ACCOUNT_NAME="devopsazuredevops"
 
 # Create Resource Group
 az group create -l uksouth -n $RESOURCE_GROUP_NAME
